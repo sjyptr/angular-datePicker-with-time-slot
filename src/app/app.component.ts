89268ef4340178db5e5aab49071fcc3dadd6fe94
sjyptr <patidarsanjay88@gmail.com>
2021-09-24 21:35:34 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,37 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 declare var $: any;
 $.datetimepicker.setLocale('en');
 
-@Component({ 
+@Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  allowdates=[];
-  myForm : FormGroup;
-allowtimes: any=[];
+  allowdates = [];
+  myForm: FormGroup;
+  allowtimes: any = [];
   constructor(private fb: FormBuilder) {}
   ngOnInit() {
     // FORM
-    this.allowdates=['24.09.2021', '23.09.2021'];
-    this.allowtimes= [
-      '12:00', '13:00', '15:00'
-     ];
+    this.allowdates = ['24.09.2021', '23.09.2021'];
+    this.allowtimes = ['12:00', '13:00', '15:00'];
     this.myForm = this.fb.group({
-       date: ['']
+      date: [''],
     });
-    
+
     $('#datetimepicker').datetimepicker({
-       inline:true,
-      lang:'en',
-      allowDates: this.allowdates, formatDate:'d.m.Y'
-   , allowTimes:this.allowtimes,
+      inline: true,
+      lang: 'en',
+      allowDates: this.allowdates,
+      formatDate: 'd.m.Y',
+      allowTimes: this.allowtimes,
 
-      onChangeDateTime: (dateText, $input) => {
-     this.myForm.controls['date'].setValue(dateText);
-     console.log(dateText.toLocaleString())
+      onChangeDateTime: (dateText) => {
+        this.myForm.controls['date'].setValue(dateText);
+        console.log(dateText.toLocaleString());
 
-     // console.log('00-0-90-90-', new Date(dateText), inst)
-    }
+        // console.log('00-0-90-90-', new Date(dateText), inst)
+      },
     });
   }
 }
